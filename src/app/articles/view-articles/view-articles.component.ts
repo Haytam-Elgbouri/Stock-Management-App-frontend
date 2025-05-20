5415b33b0f43f0dca59f12d672928e6a3eee4400
Haytam ELGBOURI <130654725+Haytam-Nagi@users.noreply.github.com>
2025-05-20 13:59:07 +0100

    created articles service and created getArticles method inside it and linked it to view articles component

--- src/app/articles/view-articles/view-articles.component.ts
+++ src/app/articles/view-articles/view-articles.component.ts
@@ -6,6 +6,7 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ConfirmDialogComponent } from '../../confirm-dialog/confirm-dialog.component';
+import { ArticlesService } from '../../services/articles.service';
 
 @Component({
   selector: 'app-view-articles',
@@ -16,67 +17,32 @@ import { ConfirmDialogComponent } from '../../confirm-dialog/confirm-dialog.comp
 export class ViewArticlesComponent implements OnInit{
 
   public displayedColumns = ["id", "reference", "designation", "family", "type", "prixUnitaireHT","action"];
-  public dataSource: any;
-  public totalElements = 0;
-  public pageSize = 2;
-  public pageIndex = 0;
+ public dataSource : any;
 
   public users : any;
 
-  constructor(private router: Router, private snackbarService : SnackbarService,   private dialog: MatDialog) { }
+  constructor(private router: Router, private snackbarService : SnackbarService,   private dialog: MatDialog, private articlesService : ArticlesService) { }
 
   @ViewChild(MatPaginator) paginator! : MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   ngOnInit(): void {
-    this.pageIndex = 0;
-    this.fetchPaginatedData();
+    this.articlesService.getUsers()
+      .subscribe({
+        next : data => {
+          this.users = data
+                   
+          this.dataSource = new MatTableDataSource(this.users);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        },
+        error : err =>{
+          const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
+          this.snackbarService.show("Erreur: " + errorMessage);
+        }
+      })
   }
   
-
-
-
-fetchPaginatedData(): void {
-  if (this.pageIndex < 0 || this.pageSize <= 0) {
-    this.snackbarService.show("Pagination invalide");
-    return;
-  }
-
-  // this.authService.getUsersPaginated(this.pageIndex, this.pageSize)
-  //   .subscribe({
-  //     next: response => {
-  //       const { content = [], totalElements = 0 } = response;
-
-  //       const usersWithFormattedRoles = content.map((user: any) => ({
-  //         ...user,
-  //         role: user.role?.startsWith("ROLE_")
-  //           ? user.role.substring(5).toLowerCase()
-  //           : user.role ?? 'inconnu'
-  //       }));
-
-  //       this.dataSource = new MatTableDataSource(usersWithFormattedRoles);
-
-  //       this.totalElements = totalElements;
-  //     },
-  //     error: err => {
-  //       const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
-  //       console.error("Erreur lors de la récupération des utilisateurs :", err);
-  //       this.snackbarService.show("Erreur : " + errorMessage);
-  //     }
-  //   });
-}
-
-
-
-  // onPageChange(event: PageEvent) {
-  //   this.pageIndex = event.pageIndex;
-  //   this.pageSize = event.pageSize;
-  //   this.fetchPaginatedData();
-  // }
-
-
-
-
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -88,48 +54,29 @@ fetchPaginatedData(): void {
       data: { name: `${element.firstName} ${element.lastName}` }
     });
   
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        // this.authService.deleteUser(element.id).subscribe({
-        //   next: () => {
-        //     this.snackbarService.show(`${element.firstName} ${element.lastName}: utilisateur supprimé avec succès`);
-        //     this.ngOnInit();
-        //   },
-        //   error: err => {
-        //     const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
-        //     this.snackbarService.show("Erreur: " + errorMessage);
-        //   }
-        // });
-      }
-    });
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if (result) {
+    //     this.authService.deleteUser(element.id).subscribe({
+    //       next: () => {
+    //         this.snackbarService.show(`${element.firstName} ${element.lastName}: utilisateur supprimé avec succès`);
+    //         this.ngOnInit();
+    //       },
+    //       error: err => {
+    //         const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
+    //         this.snackbarService.show("Erreur: " + errorMessage);
+    //       }
+    //     });
+    //   }
+    // });
   }
   
 
   onEdit(element: any) {
-    this.router.navigateByUrl(`/edit-article/${element.id}`)
-  }
-
-  changeStatus(element: any) {
-    element.isActive = !element.isActive;
-
-    const formData = {
-      isActive: element.isActive
-    }
-    // this.authService.changeUserStatus(formData, element.id).subscribe(
-    //   {
-    //     next : () => {
-  
-    //     },
-    //     error : err =>{
-    //       const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
-    //       this.snackbarService.show("Erreur: " + errorMessage);
-    //     }
-    //   }
-    // )
+    this.router.navigateByUrl(`/user/add-article/${element.id}`)
   }
 
   addUser() {
-    this.router.navigateByUrl("/user/add-article")
+    this.router.navigateByUrl("/user/add-articles")
   }
 
 }
