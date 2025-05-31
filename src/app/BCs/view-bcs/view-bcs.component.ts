0c5ac65b61224a06709abf5cb71d65da8081b345
Haytam ELGBOURI <130654725+Haytam-Nagi@users.noreply.github.com>
2025-05-31 19:49:34 +0100

    setup view bcs component

--- src/app/BCs/view-bcs/view-bcs.component.ts
+++ src/app/BCs/view-bcs/view-bcs.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { ArticlesService } from '../../services/articles.service';
+import { SnackbarService } from '../../services/snackbar.service';
+import { BcsService } from '../../services/bcs.service';
 
 @Component({
   selector: 'app-view-bcs',
@@ -8,4 +16,43 @@ import { Component } from '@angular/core';
 })
 export class ViewBCsComponent {
 
+  public displayedColumns = ['id','reference','date','supplierReference','prixTotalHT'];
+  public dataSource:any;
+
+  public BCs : any;
+
+  constructor(private router: Router, private snackbarService : SnackbarService, private bcsService : BcsService) { }
+
+  @ViewChild(MatPaginator) paginator! : MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit(): void {
+    this.bcsService.getBCs()
+      .subscribe({
+        next : data => {
+          this.BCs = data
+
+          this.dataSource = new MatTableDataSource(this.BCs);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+        },
+        error : err =>{
+          const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
+          this.snackbarService.show("Erreur: " + errorMessage);
+        }
+      })
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  consultBC(element: any) {
+    this.router.navigateByUrl(`/user/edit-article/${element.id}`)
+  }
+
+  addBC() {
+    this.router.navigateByUrl("/user/add-article")
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ViewArticlesComponent } from './articles/view-articles/view-articles.component';
 import { EditArticleComponent } from './articles/edit-article/edit-article.component';
 import { AddArticlesComponent } from './articles/add-articles/add-articles.component';
+import { ViewBCsComponent } from './BCs/view-bcs/view-bcs.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'user', pathMatch: 'full' },
@@ -12,6 +13,7 @@ const routes: Routes = [
       {path : "view-articles", component : ViewArticlesComponent},
       {path : "edit-article/:id", component : EditArticleComponent},
       {path : "add-article", component : AddArticlesComponent},
+      {path : "view-bcs", component : ViewBCsComponent},
     ]
   }
  
