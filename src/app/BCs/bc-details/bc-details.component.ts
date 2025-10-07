7fe15e70b6883dd0f1bf40d2d90fe3be1c79d229
Haytam ELGBOURI <130654725+Haytam-Nagi@users.noreply.github.com>
2025-10-07 21:43:38 +0100

    display conveniant bls in bc details page

--- src/app/BCs/bc-details/bc-details.component.ts
+++ src/app/BCs/bc-details/bc-details.component.ts
@@ -14,25 +14,36 @@ import { elementAt } from 'rxjs';
   styleUrl: './bc-details.component.css'
 })
 export class BcDetailsComponent implements OnInit{
-
+  
   constructor(private router : Router, private bcsService : BcsService, private activatedRoute : ActivatedRoute, private snackbarService : SnackbarService){}
   public bcs!: any;
   public dataSource:any;
-  public displayedColumns = ['reference', 'designation', 'prixUnitaireHT', 'prixTotalLigne','color','quantity','remaining','delivered','deliver'];
+  public displayedColumns = ['reference', 'designation', 'prixUnitaireHT', 'prixTotalLigne','color','quantity','remaining','delivered'];
+  public blsDataSource:any;
+  public blsBisplayedColumns = ['reference', 'action'];
   private bcID!:number;
   public lines!:[];
-  @ViewChild(MatPaginator) paginator! : MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  public bls!:[];
+  @ViewChild(MatPaginator) bcPaginator! : MatPaginator;
+  @ViewChild(MatSort) bcSort!: MatSort;
+
+  @ViewChild(MatPaginator) blPaginator! : MatPaginator;
+  @ViewChild(MatSort) blSort!: MatSort;
 
   ngOnInit(): void {
     this.bcID = this.activatedRoute.snapshot.params['id'];
     this.bcsService.getBC(this.bcID).subscribe({
       next : data =>{
           this.bcs = data
-          this.lines = this.bcs.lignes;
+          this.lines = this.bcs.lines;
+          this.bls = this.bcs.bls;
           this.dataSource = new MatTableDataSource(this.lines);
-          this.dataSource.paginator = this.paginator;
-          this.dataSource.sort = this.sort;
+          this.dataSource.paginator = this.bcPaginator;
+          this.dataSource.sort = this.bcSort;
+
+          this.blsDataSource = new MatTableDataSource(this.bls);
+          this.blsDataSource.paginator = this.blPaginator;
+          this.blsDataSource.sort = this.blSort;
       },
       error : err =>{
           const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
@@ -41,28 +52,32 @@ export class BcDetailsComponent implements OnInit{
     })
   }
 
-  deliver(element: any){
-    const formData = {
-      id: element.id,
-      deliveredQuantity: element.deliveredQuantity
-    };
-    console.log(formData);
+  // deliver(element: any){
+  //   const formData = {
+  //     id: element.id,
+  //     deliveredQuantity: element.deliveredQuantity
+  //   };
+  //   console.log(formData);
     
-    this.bcsService.deliver(formData).subscribe({
-      next : () =>{
-        this.snackbarService.show("Delivered");
-        this.ngOnInit();
-      },
-      error : err =>{
-        const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
-        this.snackbarService.show("Erreur: " + errorMessage);
-      }
-    })
+  //   this.bcsService.deliver(formData).subscribe({
+  //     next : () =>{
+  //       this.snackbarService.show("Delivered");
+  //       this.ngOnInit();
+  //     },
+  //     error : err =>{
+  //       const errorMessage = err?.error?.message || "Une erreur inattendue s'est produite";
+  //       this.snackbarService.show("Erreur: " + errorMessage);
+  //     }
+  //   })
 
-  }
+  // }
 
 
   goBack() {
     this.router.navigateByUrl('/user/view-bcs');
+  }
+  
+  consultBL(element: any) {
+    this.router.navigateByUrl(`/user/bl-details/${element.id}`)
   }
 }
